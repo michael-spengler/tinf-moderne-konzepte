61281db2216e7ed54b525814de05d3b938f14a33
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2023-03-14 15:52:18 +0100

    Update telegram-connector.ts

--- backend/telegram-connector.ts
+++ backend/telegram-connector.ts
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '6141574049:AAEbaspYJG6f-uUDxP4WO6s9uDeCdIsWMAY';
+const token = 'placeyourtokento.envfileandreaditfromhere';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
